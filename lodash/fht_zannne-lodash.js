d31d7f2b9e081530d5a48d1984d45e2fb01aa587
Fht-Zannne <2541090588@qq.com>
2021-07-12 19:59:24 +0800

    Signed-off-by: Fht-Zannne <2541090588@qq.com>

--- lodash/fht_zannne-lodash.js
+++ lodash/fht_zannne-lodash.js
@@ -1,77 +1,21 @@
 
 var fht_zannne = function () {
-
-    function chunk(array, n) {
-        var temp = 0
-        var arr = []
+    function chunk(array, size = 1) {
         var result = []
-        var count = 0
-        while (temp < array.length) {
-            for (let i = 0; i < n; i++) {
-                temp++
-                if (temp = array.length) {
-                    break
-                }
-                arr[i] = array[temp]
-            }
-            result[count++] = arr[i]
-            arr = []
+        var start = 0
+        var a = size
+        for (let i = 0; i < array.length; i += a) {
+            result.push(array.slice(start, size))
+            start = size
+            size += a
         }
         return result
     }
 
-
-    function compact(array) {
-        var res = []
-        for (let i = 0; i < array.length; i++) {
-
-            if (array[i]) {
-                res.push(array[i])
-            }
-        }
-        return res
+    return {
+        chunk: chunk
     }
-
-
-
-
-    function uniq(array) {
-        var log = []
-        var res = []
-        for (let i = 0; i < array.length; i++) {
-            temp = array[i]
-            if (log[temp] == null) {
-                res.push(temp)
-                log[temp] = 1
-            } else {
-                continue
-            }
-        }
-        return res
-    }
-
-
-    function uniqBy(array, f) {
-        var log = []
-        var res = []
-        for (let i = 0; i < array.length; i++) {
-            var temp = array[i]
-            if (flog[f(temp)] == null) {
-                res.push(temp)
-                log[f(temp)] = 1
-            } else {
-                continue
-            }
-        }
-        return res
-    }
-
-
-
-
-
-}
-
+}()
 
 
 
