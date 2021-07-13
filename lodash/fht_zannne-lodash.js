f978e7f899519b10aae90f8a7edb16180ea57d67
fht-zannne <83771404+fht-zannne@users.noreply.github.com>
2021-07-13 21:50:43 +0800

    Update fht_zannne-lodash.js

--- lodash/fht_zannne-lodash.js
+++ lodash/fht_zannne-lodash.js
@@ -157,7 +157,7 @@ var fht_zannne = function () {
         return res
     }
 
-    
+
     function values(obj) {
         var res = []
         for (var i in obj) {
@@ -171,8 +171,147 @@ var fht_zannne = function () {
 
 
 
+    }
 
 
+    function fill(array, value = "", start = 0, end = array.length) {
+        for (let i = 0; i < end; i++) {
+            array[i] = value
+        }
+        return array
+    }
+
+
+    function isEqual(a, b) {
+        //是否是同一对象
+        if (a === b) {
+            return true
+        }
+
+        //都是对象
+        if (typeof (a) == "object" && typeof (b) == "object") {
+            if (Object.keys(a).length != Object.keys(b).length) {
+                return false
+            }
+
+            for (var i in a) {
+                return isEqual(a[i], b[i])
+            }
+        }
+        else {
+            if (a.toString() != b.toString()) {
+                return false
+            }
+        }
+        return true
+    }
+
+
+
+    function reverse(array) {
+        if (array.length <= 1) {
+            return array
+        }
+
+        var res = []
+
+        for (let i = array.length - 1; i >= 0; i--) {
+            res.push(array[i])
+        }
+        return res
+    }
+
+
+    function countBy(coll, f) {
+
+        var check = (typeof (f) == "function")
+        var res = {}
+
+        for (var i in coll) {
+            var temp = check ? f(coll[i]) : coll[i][f]
+            if (res[temp] == null) {
+                res[temp] = []
+            }
+            res[temp].push(coll[i])
+        }
+
+        return res
+    }
+
+
+    function isNaN(val) {
+        if (val !== val) {
+            return true
+        }
+
+        if (val == null) {
+            return false
+        }
+
+        if (val.valueOf() !== val.valueOf()) {
+            return true
+        }
+
+        return false
+    }
+
+
+    function isNull(val) {
+        return null === val
+    }
+
+
+    function isNil(val) {
+        return val == undefined
+    }
+
+
+    function isUndefined(val) {
+        return val === undefined
+    }
+
+    // 输入：toArray({"a":1,"b":2})
+    // 输出/期望：[1,2]
+    // =================
+    // 输入：toArray("abc")
+    // 输出/期望：["a","b","c"]
+    // =================
+    // 输入：toArray(1)
+    // 输出/期望：[]
+    // =================
+    // 输入：toArray(null)
+    // 输出/期望：[]
+    function toArray(val) {
+        var res = []
+        if (typeof (val) == "object" || typeof (val) == "string") {
+            for (var i in val) {
+                res.push(val[i])
+            }
+        }
+        return res
+    }
+
+
+    function sum(array) {
+        var sum = 0
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i]
+        }
+        return sum
+    }
+
+
+    function sumBy(array, f) {
+        var check = (typeof (f) == 'function')
+        var sum = 0
+        for (let i = 0; i < array.length; i++) {
+            if (check) {
+                sum += f(array[i])
+            } else {
+                sum += array[i][f]
+            }
+        }
+        return sum
     }
 
 
@@ -190,22 +329,17 @@ var fht_zannne = function () {
         filter: filter,
         keys: keys,
         values: values,
+        fill: fill,
+        reverse: reverse,
+        countBy: countBy,
+        isNaN: isNaN,
+        isNull: isNull,
+        isNil: isNil,
+        isUndefined: isUndefined,
+        toArray: toArray,
+        sum: sum,
+        sumBy: sumBy,
+
 
     }
 }()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
